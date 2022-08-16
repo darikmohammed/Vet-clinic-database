a1e51a8593186da483c19d4a67e25edb83ca37c9
darikmohammed <r.m.darik@gmail.com>
2022-08-16 15:54:35 +0300

    Insert new data in visits and owners table

--- data.sql
+++ data.sql
@@ -81,3 +81,9 @@ INSERT INTO VISITS (ANIMALS_ID, VETS_ID, VISIT_DATE) SELECT ANIMALS.ID, VETS.ID 
 INSERT INTO VISITS (ANIMALS_ID, VETS_ID, VISIT_DATE) SELECT ANIMALS.ID, VETS.ID , '2020-08-03' FROM ANIMALS INNER JOIN VETS ON ANIMALS.NAME = 'Boarmon' AND VETS.NAME = 'Maisy Smith';
 INSERT INTO VISITS (ANIMALS_ID, VETS_ID, VISIT_DATE) SELECT ANIMALS.ID, VETS.ID , '2020-05-24' FROM ANIMALS INNER JOIN VETS ON ANIMALS.NAME = 'Blossom' AND VETS.NAME = 'Stephanie Mendez';
 INSERT INTO VISITS (ANIMALS_ID, VETS_ID, VISIT_DATE) SELECT ANIMALS.ID, VETS.ID , '2021-01-11' FROM ANIMALS INNER JOIN VETS ON ANIMALS.NAME = 'Blossom' AND VETS.NAME = 'William Tatche';
+
+-- This will add 3.594.280 visits considering you have 10 animals, 4 vets, and it will use around ~87.000 timestamps (~4min approx.)
+INSERT INTO visits (animals_id, vets_id, date_of_visit) SELECT * FROM (SELECT id FROM animals) animal_ids, (SELECT id FROM vets) vets_ids, generate_series('1980-01-01'::timestamp, '2021-01-01', '4 hours') visit_timestamp;
+
+-- This will add 2.500.000 owners with full_name = 'Owner <X>' and email = 'owner_<X>@email.com' (~2min approx.)
+insert into owners (full_name, email) select 'Owner ' || generate_series(1,2500000), 'owner_' || generate_series(1,2500000) || '@mail.com';
